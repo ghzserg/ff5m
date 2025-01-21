20722aac0f56a849b04a15bb7c079762bd4bdd92
Sergei Rozhkov <fgup@zserg.ru>
2025-01-21 19:55:49 +0500

    Z

--- .shell/checkMD5.sh
+++ .shell/checkMD5.sh
@@ -17,14 +17,13 @@ fi
 
 DELETE_FILE=${2}
 if [ -z "${DELETE_FILE}" ]; then
-  DELETE_FILE="false"
+    DELETE_FILE="false"
 fi
 
 ORIG_MD5="$(/usr/bin/awk -F: '/; MD5/{printf("%s", $2)}' "${FILE_NAME}"|/usr/bin/tr -d ' \r')"
-if [ -z "${ORIG_MD5}" ]
-    then
-        send_klipper 3
-        exit 3
+if [ -z "${ORIG_MD5}" ]; then
+    send_klipper 3
+    exit 3
 fi
 
 LOCAL_MD5="$(/bin/cat "${FILE_NAME}"|/bin/grep -v '^; MD5:'|/usr/bin/md5sum|/usr/bin/tr -d ' -')"
@@ -39,12 +38,11 @@ if [ "_${LOCAL_MD5}" = "_${ORIG_MD5}" ]; then
             send_klipper 5
         fi
 else
-    if [ "true" = "${DELETE_FILE}" ] || [ "True" = "${DELETE_FILE}" ]
-        then
-            /bin/rm -f "${FILE_NAME}"
-                send_klipper 6
-        else
-                send_klipper 7
+    if [ "true" = "${DELETE_FILE}" ] || [ "True" = "${DELETE_FILE}" ]; then
+        /bin/rm -f "${FILE_NAME}"
+        send_klipper 6
+    else
+        send_klipper 7
     fi
 fi
 exit 0
