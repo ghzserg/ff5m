2877e93a270ae35e79767da2db4670be4f99b943
Sergei Rozhkov <fgup@zserg.ru>
2025-01-22 10:39:46 +0500

    Zprint

--- .shell/zprint.sh
+++ .shell/zprint.sh
@@ -4,7 +4,7 @@
 # "printerSerialNumber"
 # Adventurer5M.json
 
-if [ $# -ne 2 ] && [ $# -ne 3 ]; then echo "Используйте $0 PRINT|CLOSE FILE [PRECLEAR]"; exit 1; fi
+if [ $# -ne 2 ]; then echo "Используйте $0 PRINT|CLOSE FILE"; exit 1; fi
 
 if ! [ -f /THIS_IS_NOT_YOUR_ROOT_FILESYSTEM ]; then
     CURL="/usr/bin/curl"
@@ -18,45 +18,27 @@ if [ "$ip" == "" ]; then ip=$(ip addr | grep inet | grep eth0 | awk -F" " '{prin
 serialNumber=$(cat /opt/config/Adventurer5M.json | grep "printerSerialNumber"| cut  -d ":" -f2| awk '{print $1}' | sed 's|[",]||g')
 checkCode=$(cat /opt/config/Adventurer5M.json | grep "lanCode"| cut  -d ":" -f2| awk '{print $1}' | sed 's|[",]||g')
 
-if [ "$1" == "CLOSE" ]
-    then
-        $CURL -m 60 -s \
+if [ "$1" == "CLOSE" ]; then
+    $CURL -m 60 -s \
         http://$ip:8898/control \
         -H 'Content-Type: application/json' \
         -d "{\"serialNumber\":\"$serialNumber\",\"checkCode\":\"$checkCode\",\"payload\":{\"cmd\":\"stateCtrl_cmd\",\"args\":{\"action\":\"setClearPlatform\"}}}" || \
-        echo "Нет ответа от принтера с IP $ip. Необходимо настроить принтер. На экране принтера: \"Настройки\" -> \"Иконка WiFi\" -> \"Сетевой режим\" -> включить ползунок \"Только локальные сети\""
+    echo "Нет ответа от принтера с IP $ip. Необходимо настроить принтер. На экране принтера: \"Настройки\" -> \"Иконка WiFi\" -> \"Сетевой режим\" -> включить ползунок \"Только локальные сети\""
 else
-    if [ "$1" == "PRINT" ]
-        then
-            M109=$(head -1000 "/data/$2" | grep "^M109" | head -1)
-            [ "$M109" == "" ] && M109=$(head -1000 "/data/$2" | grep "^M104" | head -1 | sed 's|M104|M109|')
-            M190=$(head -1000 "/data/$2" | grep "^M190" | head -1)
-            [ "$M190" == "" ] && M190=$(head -1000 "/data/$2" | grep "^M140" | head -1 | sed 's|M140|M190|')
-
-            if [ "$M190" == "" ] || [ "$M109" == "" ]
-                then
-                    echo "RESPOND TYPE=error MSG=\"В файле $2 не найдены команды нагрева стола(M140/M190) или сопла(M104/M109).\"" >/tmp/printer
-                    exit 1
-            fi
-
-            if [ "$3" == "PRECLEAR" ]
-                then
-                    echo "$M190" >/tmp/printer
-                    echo "$M109" >/tmp/printer
-                    echo "_START_PRECLEAR" >/tmp/printer
-                    echo "RUN_SHELL_COMMAND CMD=zprint PARAMS=\"PRINT '$2'\"">/tmp/printer
-                else
-                    echo "EXCLUDE_OBJECT_DEFINE RESET=1" >/tmp/printer 2>/dev/null
-                    head -1000 "/data/$2" | grep ^EXCLUDE_OBJECT_DEFINE >/tmp/printer 2>/dev/null
-
-                    $CURL -m 60 -s \
-                        http://$ip:8898/printGcode \
-                        -H 'Content-Type: application/json' \
-                        -d "{\"serialNumber\":\"$serialNumber\",\"checkCode\":\"$checkCode\",\"fileName\":\"$2\",\"levelingBeforePrint\":true}'" || \
-                        echo "Нет ответа от принтера с IP $ip. Необходимо настроить принтер. На экране принтера: \"Настройки\" -> \"Иконка WiFi\" -> \"Сетевой режим\" -> включить ползунок \"Только локальные сети\""
-            fi
-        else
-            echo "Используйте $0 PRINT|CLOSE FILE [PRECLEAR]"
+    if [ "$1" == "PRINT" ]; then
+        if ! [ -f "/data/$2" ]; then
+            echo "RESPOND TYPE=error MSG=\"Файл $2 не найден.\"" >/tmp/printer
+            echo "CANCEL_PRINT" >/tmp/printer
             exit 1
+        fi
+
+        $CURL -m 60 -s \
+            http://$ip:8898/printGcode \
+            -H 'Content-Type: application/json' \
+            -d "{\"serialNumber\":\"$serialNumber\",\"checkCode\":\"$checkCode\",\"fileName\":\"$2\",\"levelingBeforePrint\":true}'" || \
+            echo "Нет ответа от принтера с IP $ip. Необходимо настроить принтер. На экране принтера: \"Настройки\" -> \"Иконка WiFi\" -> \"Сетевой режим\" -> включить ползунок \"Только локальные сети\""
+    else
+        echo "Используйте $0 PRINT|CLOSE FILE [PRECLEAR]"
+        exit 1
     fi
 fi
