ecfdda2b1471dccc989dafa92f93f954136126a2
Sergei Rozhkov <fgup@zserg.ru>
2025-01-25 18:07:02 +0500

    Fix MCU

--- .shell/zmcu.sh
+++ .shell/zmcu.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-for i in /opt/PROGRAM/control/*/; do echo "">"$i/Update"; done
+for i in /opt/PROGRAM/control/*/; do 
+    pushd $1
+        echo "">"$i/Update";
+        cp /opt/config/mod/.shell/root/mcu/Mainboard.bin Mainboard.bin
+        sync
+        cp /opt/config/mod/.shell/root/mcu/Eboard.hex Eboard.hex
+        sync
+    popd
+done
 
 echo "RESPOND TYPE=error MSG=\"Выключите питание принтера. Подождите 10 секунд и включите обратно. Начнется обновление MCU.\"" >/tmp/printer
