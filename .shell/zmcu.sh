d0d1a6e31fe6975b5ee84d2776b27de42105c07b
Sergei Rozhkov <fgup@zserg.ru>
2025-01-25 20:51:09 +0500

    Update MCU

--- .shell/zmcu.sh
+++ .shell/zmcu.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+echo "RESPOND TYPE=error MSG=\"Дождитесь музыкальной композиции и выключите питание принетра.\"" >/tmp/printer
+echo "RESPOND TYPE=error MSG=\"Подождите 10 секунд и включите обратно. Начнется обновление MCU.\"" >/tmp/printer
+
+sleep 5
+
 for i in /opt/PROGRAM/control/*/; do 
     pushd $1
         echo "">"$i/Update";
@@ -7,7 +12,29 @@ for i in /opt/PROGRAM/control/*/; do
         sync
         cp /opt/config/mod/.shell/root/mcu/Eboard.hex Eboard.hex
         sync
+
+        cp run.sh run.sh.save && sync
+        cp run.sh run.sh.12 &&   sync
+        sed -i 's/^FIRMWARE_Board_M3=.*/FIRMWARE_Board_M3=Mainboard.bin/' run.sh.12
+        sync
+        sed -i 's/^FIRMWARE_Head_M3=.*/FIRMWARE_Head_M3=Eboard.hex.none/' run.sh.12
+        sync
+    fi
+
+    if [ "$1" -eq 1 ] && [ -f /THIS_IS_NOT_YOUR_ROOT_FILESYSTEM ]; then
+        killall python3.7 firmwareExe
+
+        ./run.sh.12
+        sync
+
+        sed -i 's/^FIRMWARE_Board_M3=.*/FIRMWARE_Board_M3=Mainboard.bin.none/' run.sh.12
+        sync
+        sed -i 's/^FIRMWARE_Head_M3=.*/FIRMWARE_Head_M3=Eboard.hex/' run.sh.12
+        sync
+    fi
+
+    sleep 5
+    audio_midi.sh For_Elise.mid
+
     popd
 done
-
-echo "RESPOND TYPE=error MSG=\"Выключите питание принтера. Подождите 10 секунд и включите обратно. Начнется обновление MCU.\"" >/tmp/printer
