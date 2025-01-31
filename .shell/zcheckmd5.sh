f1ab26a14601d5ba230188f15ca0c4fe4891a1a7
Sergei Rozhkov <fgup@zserg.ru>
2025-01-31 21:28:47 +0500

    MD5

--- .shell/zcheckmd5.sh
+++ .shell/zcheckmd5.sh
@@ -2,6 +2,6 @@
 
 echo "Началась проверка. Она может занять много времени...."
 cd /
-find . -name md5sum.list | while read a; do b=$(pwd); c=$(echo $a|sed 's/md5sum.list//'); echo "$c"; md5sum -c md5sum.list | grep -v -e "OK$"; cd "$b"; done
+find . -name md5sum.list | while read a; do b=$(pwd); c=$(echo $a|sed 's/md5sum.list//'); echo "$c"; cd "$c"; md5sum -c md5sum.list | grep -v -e "OK$"; cd "$b"; done
 md5sum -c /opt/config/mod/.shell/md5sum.list | grep -v -e "OK$"
 echo "Оригиналы файлов можно найти по ссылке https://github.com/ghzserg/zmod/tree/main/stock"
