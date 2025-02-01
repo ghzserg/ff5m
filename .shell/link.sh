f3edf080f9f4539b40d276dce2df4251d175c732
Sergei Rozhkov <fgup@zserg.ru>
2025-02-01 16:28:40 +0500

    synlink

---
+++ .shell/link.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp -a "$1" "../stock/$1"
+a=$(readlink "$1")
+echo "if [ \"\$(readlink '$1')\" != \"$a\" ]; then echo \"$1 - Ошибочная ссылка: \"; rm -f \"$1\"; ln -s \"$a\" \"$1\" && echo \"Исправлено\"; fi"
