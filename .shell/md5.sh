660afec4f6000d06bb04463e2e43bd8825b109e2
Sergei Rozhkov <fgup@zserg.ru>
2025-02-03 09:00:55 +0500

    Direcrtory

--- .shell/md5.sh
+++ .shell/md5.sh
@@ -115,6 +115,50 @@ find .  \
 
 echo "echo 'Проверяю права...'">>list.link
 find .  \
+    -type d \
+    -and -not -name "*.pyc" \
+    -and -not -path "./dev*" \
+    -and -not -path "./run*" \
+    -and -not -path "*__pycache__*" \
+    -and -not -name "md5sum.list" \
+    -and -not -name "md5.sh" \
+    -and -not -name "link.sh" \
+    -and -not -name "list.link" \
+    -and -not -path "./.rnd" \
+    -and -not -path "./.wpa_cli_history" \
+    -and -not -path "./root/printer_data/version.txt" \
+    -and -not -path "./etc/dropbear/*" \
+    -and -not -path "./etc/timezone" \
+    -and -not -path "./etc/localtime" \
+    -and -not -path "./opt/config/*" \
+    -and -not -path "./opt/PROGRAM/control/*" \
+    -and -not -path "./opt/PROGRAM/kernel/*" \
+    -and -not -path "./opt/PROGRAM/library/*" \
+    -and -not -path "./opt/PROGRAM/software/*" \
+    -and -not -path "./opt/auto_run.sh" \
+    -and -not -path "./root/version" \
+    -and -not -path "./root/.viminfo" \
+    -and -not -path "./opt/klipper/klippy/extras/spi_temperature.py" \
+    -and -not -path "./opt/klipper/klippy/extras/gcode_shell_command.py" \
+    -and -not -path "./opt/klipper/klippy/extras/buttons.py" \
+    -and -not -path "./opt/klipper/klippy/extras/save_variables.py" \
+    -and -not -path "./opt/klipper/klippy/mcu.py" \
+    -and -not -path "./opt/klipper/klippy/toolhead.py" \
+    -and -not -path "./opt/key.priv" \
+    -and -not -path "./opt/private.pem" \
+    -and -not -path "./opt/key.pub" \
+    -and -not -path "./etc/shadow-" \
+    -and -not -path "./etc/shadow" \
+    -and -not -path "./etc/rtl_hostapd_2G.conf" \
+    -and -not -path "./etc/hosts" \
+    -and -not -path "./etc/random-seed" \
+    -and -not -path "./etc/MAC" \
+    -and -not -path "./etc/wpa_supplicant.conf" \
+    -and -not -path "./opt/tslib-1.12/etc/pointercal" \
+    -and -not -path "./Settings/Trolltech.conf" \
+    -exec ./link.sh {} "file" \; >>list.link
+
+find .  \
     -type f \
     -and -not -name "*.pyc" \
     -and -not -path "./dev*" \
