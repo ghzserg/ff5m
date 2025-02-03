c4f2f4ef36b3c53527b5c476b954d4ebe08a22f5
Sergei Rozhkov <fgup@zserg.ru>
2025-02-03 11:10:32 +0500

    znozzle.sh

--- .shell/znozzle.sh
+++ .shell/znozzle.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-echo "[temperature_sensor weightValue]
+if [ "$1" == "0" ]; then
+    echo "">/opt/config/mod_data/nozzle.cfg
+else
+    echo "[temperature_sensor weightValue]
 max_temp: $1" >/opt/config/mod_data/nozzle.cfg
+fi
+
 sync
 sleep 5
 sync
