361d7efa81851ff7b3dabfb8697208da12cb8a06
Sergei Rozhkov <fgup@zserg.ru>
2025-02-05 11:02:41 +0500

    SCV

--- .shell/root/zshaper.sh
+++ .shell/root/zshaper.sh
@@ -6,8 +6,18 @@ sed 's/psd_x/psd_Y/' calibration_data_x_*.csv | sed 's/psd_y/psd_x/' | sed 's/ps
 sed 's/psd_x/psd_Y/' calibration_data_y_*.csv | sed 's/psd_y/psd_x/' | sed 's/psd_Y/psd_y/' | awk -F ',' '{print $1","$3","$2","$4","$5","$6","$7","$8","$9","$10}' >Y
 
 SCV="5.0"
-grep -q '^square_corner_velocity' /opt/config/printer.base.cfg && SCV=$(grep '^square_corner_velocity' /opt/config/printer.base.cfg| cut -d ":" -f 2 | awk '{print $1}')
-grep -q '^square_corner_velocity' /opt/config/mod_data/user.cfg && SCV=$(grep '^square_corner_velocity' /opt/config/mod_data/user.cfg| cut -d ":" -f 2 | awk '{print $1}')
+if grep -q "fix_scv = 1" /opt/config/mod_data/variables.cfg; then
+    if grep -q '^square_corner_velocity' /opt/config/mod_data/user.cfg; then
+        SCV=$(grep '^square_corner_velocity' /opt/config/mod_data/user.cfg| cut -d ":" -f 2 | awk '{print $1}')
+        echo "Используется SCV из mod_data/user.cfg"
+    else if grep -q '^square_corner_velocity' /opt/config/printer.base.cfg; then
+        SCV=$(grep '^square_corner_velocity' /opt/config/printer.base.cfg| cut -d ":" -f 2 | awk '{print $1}')
+        echo "Используется SCV из printer.base.cfg"
+    else
+        echo "Используется стандартный SCV"
+    fi
+    fi
+fi
 
 echo "SCV (square_corner_velocity) = $SCV"
 
