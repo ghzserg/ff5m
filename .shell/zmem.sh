d33ccf124a924cadfb8b86a977087ae48af95bd0
Sergei Rozhkov <fgup@zserg.ru>
2025-02-06 13:21:27 +0500

    MEM

--- .shell/zmem.sh
+++ .shell/zmem.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-/root/printer_data/scripts/ps_mem.py | sed 's/python3.7/Klipper/' | sed 's/python3.11/Moonraker/' | sed 's/firmwareExe/Экран/' | sed 's/mjpg_streamer/Камера/'
+
+/root/printer_data/scripts/ps_mem.py >/tmp/list.txt
+sed 's/python3.7/Klipper/' /tmp/list.txt | sed 's/python3.11/Moonraker/' | sed 's/firmwareExe/Экран/' | sed 's/mjpg_streamer/Камера/'
+rm -f /tmp/list.txt
 free -m
 echo "Процессы в SWAP:"
 for file in /proc/*/status ; do awk '/VmSwap|Name/{printf $2 " " $3}END{ print ""}' $file; done |sort -k 2 -n -r|head -8| sed 's/python3.7/Klipper/' | sed 's/python3/Moonraker/' | sed 's/firmwareExe/Экран/' | sed 's/mjpg_streamer/Камера/'
