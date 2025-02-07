e9446f8c3391b1c15aa6c76b50c8dd81236c1bc5
Sergei Rozhkov <fgup@zserg.ru>
2025-02-07 21:26:02 +0500

    guppy

--- .shell/root/moonraker/components/klippy_connection.py
+++ .shell/root/moonraker/components/klippy_connection.py
@@ -625,6 +625,8 @@ class KlippyConnection:
                 raise self.server.error(
                     "No connection associated with subscription request"
                 )
+            # if the connection has an existing subscription pop it off
+            self.subscriptions.pop(conn, None)
             requested_sub: Subscription = args.get('objects', {})
             all_subs: Subscription = dict(requested_sub)
             # Build the subscription request from a superset of all client subscriptions
@@ -696,7 +698,8 @@ class KlippyConnection:
                 if obj_name not in all_status:
                     del self.subscription_cache[obj_name]
             result['status'] = pruned_status
-            self.subscriptions[conn] = requested_sub
+            if requested_sub:
+                self.subscriptions[conn] = requested_sub
             return result
 
     async def _request_standard(
--- .shell/zdisplay.sh
+++ .shell/zdisplay.sh
@@ -4,12 +4,12 @@ if ! [ $# -eq 1 ]; then echo "Use $0 on|off|test"; exit 1; fi
 
 if [ $1 = "test" ] && grep -q display_off.cfg /opt/config/printer.cfg; then
     killall firmwareExe
-    grep -q "guppi = 1" /opt/config/mod_data/variables.cfg && /opt/config/.shell/zguppi.sh start || xzcat /opt/config/mod/.shell/screen_off.raw.xz > /dev/fb0
+    grep -q "guppy = 1" /opt/config/mod_data/variables.cfg && /opt/config/mod/.shell/zguppy.sh start || xzcat /opt/config/mod/.shell/screen_off.raw.xz > /dev/fb0
 fi
 
 [ $1 = "on" ]   && sed -i 's|\[include ./mod/display_off.cfg\]|\[include ./mod/mod.cfg\]|' /opt/config/printer.cfg && sync && reboot
 [ $1 = "off" ]  && sed -i 's|\[include ./mod/mod.cfg\]|\[include ./mod/display_off.cfg\]|' /opt/config/printer.cfg && sync && killall firmwareExe && xzcat /opt/config/mod/.shell/screen_off.raw.xz > /dev/fb0
-[ $1 = "guppi" ]  && sed -i 's|\[include ./mod/mod.cfg\]|\[include ./mod/display_off.cfg\]|' /opt/config/printer.cfg && sync && killall firmwareExe && /opt/config/.shell/zguppi.sh start
+[ $1 = "guppy" ]  && sed -i 's|\[include ./mod/mod.cfg\]|\[include ./mod/display_off.cfg\]|' /opt/config/printer.cfg && sync && killall firmwareExe && /opt/config/mod/.shell/zguppy.sh start
 
 sync
 echo 3 > /proc/sys/vm/drop_caches
