7f24c0420029e3178f5141658be1b326f9b6f818
Sergei Rozhkov <fgup@zserg.ru>
2025-02-17 21:16:51 +0500

    guppy

--- .shell/zshaper_new.sh
+++ .shell/zshaper_new.sh
@@ -7,7 +7,7 @@ unset LD_PRELOAD
 if ! [ -f /THIS_IS_NOT_YOUR_ROOT_FILESYSTEM ]; then
     /opt/config/mod/.shell/root/zshaper_new.sh $@
 else
-    umount /data/.mod/
+    while umount /data/.mod/ 2>/dev/null; do a=b; done
     chroot $MOD /opt/config/mod/.shell/root/zshaper_new.sh $@
     mount --bind /data/lost+found /data/.mod
 fi
