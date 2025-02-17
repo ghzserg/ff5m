9f2d311cc6185b376f0b9cab68f55080aede6925
Sergei Rozhkov <fgup@zserg.ru>
2025-02-17 20:23:51 +0500

    ZSHAPER

--- .shell/root/zshaper/calibrate_shaper.py
+++ .shell/root/zshaper/calibrate_shaper.py
@@ -68,8 +68,8 @@ def calibrate_shaper(datas, csv_output, *, shapers, damping_ratio, scv,
               (','.join(shapers)))
         return None, None, None
     if send_klipper == "X" or send_klipper == "Y":
-        with open('/tmp/print', 'a') as file:
-            file.write("SET_INPUT_SHAPER SHAPER_FREQ_%s=%s SHAPER_TYPE_%s=%s" % (send_klipper, shaper.name, send_klipper, shaper.freq))
+        with open('/tmp/printer', 'a') as file:
+            file.write("SET_INPUT_SHAPER SHAPER_FREQ_%s=%s SHAPER_TYPE_%s=%s\n" % (send_klipper, shaper.name, send_klipper, shaper.freq))
     if resp_json == 0.0:
         print("Рекомендуемый шейпер %s @ %.1f Hz" % (shaper.name, shaper.freq))
     if csv_output is not None:
--- .shell/root/zshaper_new.sh
+++ .shell/root/zshaper_new.sh
@@ -14,4 +14,25 @@ if grep -q "fix_scv = 1" /opt/config/mod_data/variables.cfg; then
     fi
 fi
 
-python3 /opt/config/mod/.shell/root/zshaper/calibrate_shaper.py $@ --scv=$SCV
+DT=$(date '+%Y%m%d_%H%M')
+
+cd /opt/config/mod_data/
+
+sed 's/psd_x/psd_Y/' /tmp/resonances_x_X.csv | sed 's/psd_y/psd_x/' | sed 's/psd_Y/psd_y/' | awk -F ',' '{print $1","$3","$2","$4","$5"}' >X
+sed 's/psd_x/psd_Y/' /tmp/resonances_y_Y.csv | sed 's/psd_y/psd_x/' | sed 's/psd_Y/psd_y/' | awk -F ',' '{print $1","$3","$2","$4","$5"}' >Y
+
+echo "Подготовка изображения оси X. Ждите..."
+python3 /opt/config/mod/.shell/root/zshaper/calibrate_shaper.py X --scv=$SCV -o calibration_data_X_$DT.png -w 8 -l 4.8 --send X
+cp calibration_data_X_$DT.png resonances_x.png
+
+echo "Подготовка изображения оси Y. Ждите..."
+python3 /opt/config/mod/.shell/root/zshaper/calibrate_shaper.py Y --scv=$SCV -o calibration_data_Y_$DT.png -w 8 -l 4.8 --send Y
+cp calibration_data_Y_$DT.png resonances_y.png
+
+mkdir -p shapers
+mv X_$DT shapers/X_$DT.csv
+mv Y_$DT shapers/Y_$DT.csv
+mv calibration_data_X_$DT.png shapers/
+mv calibration_data_Y_$DT.png shapers/
+
+echo "Изображения лежат во вкладке Конфигурация -> mod_data. resonances_x.png и resonances_y.png."
