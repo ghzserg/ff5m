e7c19b9c7647257332866195086d7e426c64117c
Sergei Rozhkov <fgup@zserg.ru>
2025-02-17 20:43:18 +0500

    ZSHAPER

--- .shell/root/zshaper_new.sh
+++ .shell/root/zshaper_new.sh
@@ -22,17 +22,17 @@ sed 's/psd_x/psd_Y/' /tmp/resonances_x_X.csv | sed 's/psd_y/psd_x/' | sed 's/psd
 sed 's/psd_x/psd_Y/' /tmp/resonances_y_Y.csv | sed 's/psd_y/psd_x/' | sed 's/psd_Y/psd_y/' | awk -F ',' '{print $1","$3","$2","$4","$5}' >Y
 
 echo "Подготовка изображения оси X. Ждите..."
-python3 /opt/config/mod/.shell/root/zshaper/calibrate_shaper.py X --scv=$SCV -o calibration_data_X_$DT.png -w 8 -l 4.8 --send X
+python3 /opt/config/mod/.shell/root/zshaper/calibrate_shaper.py X --scv=$SCV -o resonances_x.png -w 8 -l 4.8 --send X
 cp calibration_data_X_$DT.png resonances_x.png
 
 echo "Подготовка изображения оси Y. Ждите..."
-python3 /opt/config/mod/.shell/root/zshaper/calibrate_shaper.py Y --scv=$SCV -o calibration_data_Y_$DT.png -w 8 -l 4.8 --send Y
+python3 /opt/config/mod/.shell/root/zshaper/calibrate_shaper.py Y --scv=$SCV -o resonances_y.png -w 8 -l 4.8 --send Y
 cp calibration_data_Y_$DT.png resonances_y.png
 
 mkdir -p shapers
-mv X_$DT shapers/X_$DT.csv
-mv Y_$DT shapers/Y_$DT.csv
-mv calibration_data_X_$DT.png shapers/
-mv calibration_data_Y_$DT.png shapers/
+mv X shapers/X_$DT.csv
+mv Y shapers/Y_$DT.csv
+cp resonances_x.png shapers/calibration_data_X_$DT.png
+cp resonances_y.png shapers/calibration_data_Y_$DT.png
 
 echo "Изображения лежат во вкладке Конфигурация -> mod_data. resonances_x.png и resonances_y.png."
