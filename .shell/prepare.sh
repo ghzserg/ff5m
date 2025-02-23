80fbacf9a306cd514d7082d71626f927a64e7e59
Sergei Rozhkov <fgup@zserg.ru>
2025-02-23 19:56:06 +0500

    Fix remove

--- .shell/prepare.sh
+++ .shell/prepare.sh
@@ -13,6 +13,8 @@ restore_base()
 {
     grep -q '^\[include mod.user.cfg' /opt/config/printer.cfg && sed -i '/include mod.user.cfg/d' /opt/config/printer.cfg
     grep -q '^\[include ./mod/mod.cfg' /opt/config/printer.cfg && sed -i '/mod.cfg/d' /opt/config/printer.cfg
+    grep -q '^\[include ./mod_data/user.cfg' /opt/config/printer.cfg && sed -i '/user.cfg/d' /opt/config/printer.cfg
+    grep -q '^\[include ./mod/switch_sensor.cfg' /opt/config/printer.cfg && sed -i '/switch_sensor.cfg/d' /opt/config/printer.cfg
     grep -q '^\[include ./mod/display_off.cfg' /opt/config/printer.cfg && sed -i '/display_off.cfg/d' /opt/config/printer.cfg
 
     ns_off api.cloud.flashforge.com
